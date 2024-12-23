4e0b8e98f65ac48ec3bf2bc682f4afdd45989552
Rafael Gieschke <rafael@gieschke.de>
2024-12-23 23:09:33 +0100

    feat(memdump): allow to write instead of read

--- memdump.c
+++ memdump.c
@@ -7,9 +7,15 @@
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-  if (argc < 2 || argc > 3) {
-    fprintf(stderr, "%s 0xMemoryLocation [/dev/mem]\n", argv[0]);
+  if (argc < 2 || argc > 4) {
+    fprintf(stderr, "%s [-r] 0xMemoryLocation [/dev/mem]\n", argv[0]);
     return 2;
+  }
+
+  int revert = 0;
+  if (!strcmp(argv[1], "-r")) {
+    revert = 1;
+    argv += 1;
   }
 
   size_t target = strtoll(argv[1], 0, 0);
@@ -28,7 +34,7 @@ int main(int argc, char *argv[]) {
   char empty[MAP_SIZE];
   memset(empty, 0, sizeof empty);
 
-  int fd = open(file, O_RDONLY | O_SYNC);
+  int fd = open(file, (revert ? O_RDWR : O_RDONLY) | O_SYNC);
   if (fd == -1) {
     perror("open");
     return 1;
@@ -37,7 +43,7 @@ int main(int argc, char *argv[]) {
   int last_errno = 0;
   for (;;) {
     size_t map_start = target & ~MAP_MASK;
-    char *map = mmap(0, MAP_SIZE, PROT_READ, MAP_SHARED, fd, map_start);
+    char *map = mmap(0, MAP_SIZE, revert ? PROT_WRITE : PROT_READ, MAP_SHARED, fd, map_start);
     if (map == MAP_FAILED) {
       if (errno != last_errno) {
         last_errno = errno;
@@ -53,10 +59,29 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "mmap@0x%08lx: %s\n", map_start, strerror(last_errno));
       }
     }
-    write(1, map + (target - map_start), MAP_SIZE - (target - map_start));
+    if (revert) {
+      size_t bytes = read(0, map + (target - map_start), MAP_SIZE - (target - map_start));
+      if (bytes == -1) {
+        perror("read");
+        return 1;
+      }
+      if (bytes == 0) {
+        return 0;
+      }
+      target += bytes;
+      if (map == empty) {
+        memset(empty, 0, sizeof empty);
+      }
+    } else {
+      size_t bytes = write(1, map + (target - map_start), MAP_SIZE - (target - map_start));
+      if (bytes == -1) {
+        perror("write");
+        return 1;
+      }
+      target = map_start + MAP_SIZE;
+    }
     if (map != empty) {
       munmap(map, MAP_SIZE);
     }
-    target = map_start + MAP_SIZE;
   }
 }
