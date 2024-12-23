176e2e7c80d5d3a9e72cd3985e172db3dc065797
Rafael Gieschke <rafael@gieschke.de>
2024-12-23 18:47:04 +0100

    feat: allow to resume on read errors

--- memdump.c
+++ memdump.c
@@ -19,10 +19,14 @@ int main(int argc, char *argv[]) {
     file = argv[2];
   }
 
+  int on_error_resume_next = getenv("force") && *getenv("force");
+
   fprintf(stderr, "%s @ 0x%lx:\n", file, target);
 
   size_t MAP_SIZE = sysconf(_SC_PAGE_SIZE);
   unsigned long MAP_MASK = (MAP_SIZE - 1);
+  char empty[MAP_SIZE];
+  memset(empty, 0, sizeof empty);
 
   int fd = open(file, O_RDONLY | O_SYNC);
   if (fd == -1) {
@@ -30,15 +34,29 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
+  int last_errno = 0;
   for (;;) {
     size_t map_start = target & ~MAP_MASK;
     char *map = mmap(0, MAP_SIZE, PROT_READ, MAP_SHARED, fd, map_start);
     if (map == MAP_FAILED) {
-      fprintf(stderr, "mmap@0x%08lx: %s\n", map_start, strerror(errno));
-      return 1;
+      if (errno != last_errno) {
+        last_errno = errno;
+        fprintf(stderr, "mmap@0x%08lx: %s\n", map_start, strerror(last_errno));
+      }
+      if (!on_error_resume_next) {
+        return 1;
+      }
+      map = empty;
+    } else {
+      if (last_errno) {
+        last_errno = 0;
+        fprintf(stderr, "mmap@0x%08lx: %s\n", map_start, strerror(last_errno));
+      }
     }
     write(1, map + (target - map_start), MAP_SIZE - (target - map_start));
-    munmap(map, MAP_SIZE);
+    if (map != empty) {
+      munmap(map, MAP_SIZE);
+    }
     target = map_start + MAP_SIZE;
   }
 }
